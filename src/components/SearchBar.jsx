4f50cb119e3bdce144d64d9d4b3cc06e7537fbcb
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-09-06 14:18:25 -0300

    requisitos 1 oa 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,29 +1,61 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label">Inclui o texto:
-          <input 
-          data-testid="text-input"
-          type="text" 
-          value={ searchText }  
-          onChange={ onSearchTextChange } />
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
         </label>
-        <label data-testid="checkbox-input-label">
-          <input 
-          data-testid="checkbox-input"
-          type="checkbox" 
-          checked= { bookmarkedOnly }
-          onChange={ onBookmarkedChange } />
+        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
           Mostrar somente favoritos
+        </label>
+        <label htmlFor="select-input-label" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            name="thelist"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
 
-    )
+    );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -117,27 +117,27 @@ describe('4 - Renderize um input do tipo checkbox dentro do formulário em `<Sea
 describe('5 - Renderize um select dentro do formulário em `<SearchBar />`', () => {
   beforeEach(() => beforeEachUnitTest());
 
-  it.skip('Renderize um select dentro do form', () => {
+  it('Renderize um select dentro do form', () => {
     const { getAllByTestId } = searchBar();
     const selectInput = getAllByTestId(selectTestId);
     expect(selectInput).toHaveLength(1);
   });
 
-  it.skip('Renderize, associada ao componente, uma label com o texto "Filtrar por gênero"', () => {
+  it('Renderize, associada ao componente, uma label com o texto "Filtrar por gênero"', () => {
     const { getAllByTestId } = searchBar();
     const selectInputLabel = getAllByTestId('select-input-label');
     expect(selectInputLabel).toHaveLength(1);
     expect(selectInputLabel[0]).toHaveTextContent('Filtrar por gênero');
   });
 
-  it.skip('Passe a prop `selectedGenre` como valor do select', () => {
+  it('Passe a prop `selectedGenre` como valor do select', () => {
     const { getByTestId } = searchBar();
     const selectInput = getByTestId(selectTestId);
 
     expect(selectInput).toHaveValue(props.selectedGenre);
   });
 
-  it.skip('Passe a prop `onSelectedGenreChange` para o atributo `onChange` do select', () => {
+  it('Passe a prop `onSelectedGenreChange` para o atributo `onChange` do select', () => {
     const { getByTestId } = searchBar();
     const selectInput = getByTestId(selectTestId);
     event.selectOptions(selectInput, 'comedy');
@@ -145,7 +145,7 @@ describe('5 - Renderize um select dentro do formulário em `<SearchBar />`', () 
     expect(props.onSelectedGenreChange).toHaveBeenCalledTimes(1);
   });
 
-  it.skip("Renderize 4 options dentro do select com os textos e valores, respectivamente: Todos e '', Ação e action, Comédia e comedy, Suspense e thriller", () => {
+  it("Renderize 4 options dentro do select com os textos e valores, respectivamente: Todos e '', Ação e action, Comédia e comedy, Suspense e thriller", () => {
     const expectedLength = 4;
     const genreOptions = [
       { text: 'Todos', value: '' },
