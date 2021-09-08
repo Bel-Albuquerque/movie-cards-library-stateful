f5b5f16cb1afb4c6a52b65cd9351630f51e91d25
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-09-07 22:14:40 -0300

    testando

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -46,6 +46,7 @@ class AddMovie extends React.Component {
   render() {
     const { onClick } = this.props;
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+
     return (
       <form data-testid="add-movie-form">
         <JuntaInputs
