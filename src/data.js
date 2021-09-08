178c78e7c9a291b495a32bd1104b7a64a77e234f
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-09-08 10:39:06 -0300

    teste

--- src/data.js
+++ src/data.js
@@ -1,31 +1,29 @@
 const movies = [
-    {
-      title: 'An awesome title',
-      subtitle: 'Movie Subtitle 1',
-      storyline: 'Movie Storyline 1',
-      rating: 4.5,
-      imagePath: 'images/movie_1',
-      bookmarked: true,
-      genre: 'action',
-    },
-    {
-      title: 'Movie Title 2',
-      subtitle: 'An incredible subtitle',
-      storyline: 'Movie Storyline 2',
-      rating: 4.5,
-      imagePath: 'images/movie_2',
-      bookmarked: false,
-      genre: 'comedy',
-    },
-    {
-      title: 'Movie Title 3',
-      subtitle: 'Movie Subtitle 3',
-      storyline: 'An great storyline',
-      rating: 3,
-      imagePath: 'images/movie_3',
-      bookmarked: false,
-      genre: 'thriller',
-    },
-  ];
-
-export default movies;
+  {
+    title: 'An awesome title',
+    subtitle: 'Movie Subtitle 1',
+    storyline: 'Movie Storyline 1',
+    rating: 4.5,
+    imagePath: 'images/movie_1',
+    bookmarked: true,
+    genre: 'action',
+  },
+  {
+    title: 'Movie Title 2',
+    subtitle: 'An incredible subtitle',
+    storyline: 'Movie Storyline 2',
+    rating: 4.5,
+    imagePath: 'images/movie_2',
+    bookmarked: false,
+    genre: 'comedy',
+  },
+  {
+    title: 'Movie Title 3',
+    subtitle: 'Movie Subtitle 3',
+    storyline: 'An great storyline',
+    rating: 3,
+    imagePath: 'images/movie_3',
+    bookmarked: false,
+    genre: 'thriller',
+  },
+];
